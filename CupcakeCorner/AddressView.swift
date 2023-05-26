31c0524e5ea7976191bbe6d598ee486c340cda31
Paul Onawola <onawola.ire@gmail.com>
2023-05-26 15:37:34 +0100

    Cupcake Corner is an app for overing cupcakes with toppings and sprinkles when needed. The app makes use for core swiftui tools and principles

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,41 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Paul Onawola on 02/05/2023.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Check out")
+                }
+
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery Display")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView(order: Order())
+    }
+}
